0429b512f31d884a1ecf7847a2b0088da7c5642a
nghiadh <nghiadaohuu@zamba.vn>
2024-05-28 17:40:52 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import Button from "~/components/Button";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {publicRoutes} from '~/routes'
+
 function App() {
-  return (
-    <div className="App">
-      <Button/>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((routes, index) => {
+                        const Page = routes.component
+                        return <Route
+                            key={index}
+                            path={routes.path}
+                            element={<Page />}
+                        ></Route>
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
